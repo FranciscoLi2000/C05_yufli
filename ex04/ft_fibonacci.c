c0f4698ceb4db3b63275164e5dad006bbb2f4760
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-01 17:17:53 +0000

    斐波那契数列

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+#include <stdio.h>
+int	main()
+{
+	int	i;
+
+	scanf("%i", &i);
+	printf("%i\n", ft_fibonacci(i));
+	return (0);
+}
